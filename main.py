a990aa9580afef8a4540dccb8d30a803aebb3675
atul <atulgutal007@gmail.com>
2021-08-22 18:23:59 -0700

    Implemented Post method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Path
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,14 +12,25 @@ students = {
 }
 
 
+class Student(BaseModel):
+    name: str
+    age: int
+
+# Get method
+
+
 @app.get("/")
 def index():
     return {"name": "First Data"}
+
+# Path parameter
 
 
 @app.get("/students/{student_id}")
 def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt=0)):
     return students[student_id]
+
+# Query parameter
 
 
 @app.get("/get_by_name")
@@ -27,3 +39,14 @@ def get_student(name: Optional[str] = None):
         if students[student_id]["name"] == name:
             return students[student_id]
     return {"Data": "Not found"}
+
+# Post Method
+
+
+@app.post("/create_student/{student_id}")
+def create_student(student_id: int, student: Student):
+    if student_id in students:
+        return {"Error": "Student Exists"}
+
+    students[student_id] = student
+    return students[student_id]
